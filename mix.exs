e15002caa031811e98ca91ab5e2f14c1a71613a5
Fernando Mendes <fernando@mendes.codes>
2023-01-23 12:27:21 +0000

    Bump deps

--- mix.exs
+++ mix.exs
@@ -2,14 +2,14 @@ defmodule AWSAuth.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :aws_auth,
-     version: "0.7.2",
-     elixir: "~> 1.3",
-     description: description(),
-     package: package(),
-     deps: deps(),
-     test_coverage: [tool: ExCoveralls],
-     preferred_cli_env: [coveralls: :test]
+    [
+      app: :aws_auth,
+      version: "0.7.2",
+      elixir: "~> 1.13",
+      description: description(),
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [coveralls: :test]
     ]
   end
 
@@ -19,25 +19,15 @@ defmodule AWSAuth.Mixfile do
 
   defp deps do
     [
-      {:earmark, "~> 1.2.3", only: :dev },
-      {:ex_doc, "~> 0.16", only: :dev },
-      {:excoveralls, "~> 0.4", only: :test},
-      {:credo, "~> 0.8.6", only: [:dev, :test]}
+      {:ex_doc, "~> 0.29.1", only: :dev},
+      {:credo, "~> 1.5", only: [:dev, :test]}
     ]
   end
 
   defp description do
     """
-    AWS Signature Version 4 Signing Library
+    AWS Signature Version 4 Signing Library.
+    Fork used internally and mainted by Avenue.
     """
-  end
-
-  defp package do
-    [
-     files: ["lib", "mix.exs", "README*"],
-     maintainers: ["Bryan Joseph"],
-     licenses: ["Apache 2.0"],
-     links: %{"GitHub" => "https://github.com/bryanjos/aws_auth"}
-    ]
   end
 end
